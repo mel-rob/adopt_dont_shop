9553355bd9451ce64142e4d88bf270af961e4074
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-19 16:30:08 -0700

    Add test for shelters index page user_can_see_all_shelters

---
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "shelters index page", type: :feature do
+  it "can see all shelters" do
+    shelter_1 = Shelter.create(name:     "Reptile Room",
+                               address:  "2364 Desert Lane",
+                               city:     "Denver",
+                               state:    "CO",
+                               zip:      "80211")
+
+    shelter_2 = Shelter.create(name:    "Fish Farm",
+                               address: "8473 Ocean Drive",
+                               city:    "Denver",
+                               state:   "CO",
+                               zip:     "80232")
+
+    visit '/shelters'
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page). to have_content("Name: #{shelter_1.name}")
+
+    expect(page).to have_content(shelter_2.name)
+    expect(page). to have_content("Name: #{shelter_2.name}")
+  end
+end
