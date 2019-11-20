b933cd6b2483654099e352069d8586c6ffb7d84a
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-19 20:32:35 -0700

    Add new and create method

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -4,7 +4,29 @@ class SheltersController < ApplicationController
     @shelters = Shelter.all
   end
 
+  def new
+  end
+
+  def create
+    shelter = Shelter.new({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip]
+      })
+      
+    shelter.save
+
+    redirect_to '/shelters'
+  end
+
   def show
     @shelter = Shelter.find(params[:id])
   end
+
+  # private
+  # def shelter_params
+  #   params.require(:shelter).permit(:name, :address, :city, :state, :zip)
+  # end
 end
