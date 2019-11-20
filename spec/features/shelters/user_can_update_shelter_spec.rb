7f14437228281b441e5d5a35295c0217b2508e5b
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-19 21:29:15 -0700

    Add test for user_can_update_shelter

---
+++ spec/features/shelters/user_can_update_shelter_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "shelters show page", type: :feature do
+  it "can update shelter" do
+    shelter_1 = Shelter.create(name:     "Reptile Room",
+                               address:  "2364 Desert Lane",
+                               city:     "Denver",
+                               state:    "CO",
+                               zip:      "80211")
+
+    shelter_2 = Shelter.create(name:    "Fish Farm",
+                               address: "8473 Ocean Drive",
+                               city:    "Denver",
+                               state:   "CO",
+                               zip:     "80232")
+
+    visit "/shelters/#{shelter_1.id}"
+
+    expect(page).to have_link('Update')
+
+    click_link
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+
+    expect(page).to have_button('Submit')
+
+    fill_in 'name', :with => 'Rattler Room'
+
+    click_button
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}")
+  end
+end
