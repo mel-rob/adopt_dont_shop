ad9bd55437e0f831c998f17b1afb6b1fb39b37c7
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-19 21:28:54 -0700

    Add routes for GET '/shelters/:id/edit' and PATCH '/shelters/:id'

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
-  get '/shelters/:id', to: 'shelters#show'
   post '/shelters', to: 'shelters#create'
+  get '/shelters/:id', to: 'shelters#show'
+  get '/shelters/:id/edit', to: 'shelters#edit'
+  patch '/shelters/:id', to: 'shelters#update'
 end
