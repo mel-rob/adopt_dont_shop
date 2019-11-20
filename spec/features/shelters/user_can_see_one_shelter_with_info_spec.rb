879d8fba8fdc5b3c25ba02931244d136e929e9c2
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-19 20:36:13 -0700

    Refactor test for user_can_see_one_shelter_with_info

--- spec/features/shelters/user_can_see_one_shelter_with_info_spec.rb
+++ spec/features/shelters/user_can_see_one_shelter_with_info_spec.rb
@@ -17,14 +17,17 @@ RSpec.describe "shelter show page", type: :feature do
     visit "/shelters/#{shelter_1.id}"
 
     expect(page).to have_content(shelter_1.name)
-    expect(page).to have_content("#{shelter_1.name}")
     expect(page).to have_content("Address: #{shelter_1.address}")
     expect(page).to have_content("City: #{shelter_1.city}")
     expect(page).to have_content("State: #{shelter_1.state}")
     expect(page).to have_content("Zip: #{shelter_1.zip}")
 
-    # visit '/shelters/2'
-    # expect(page).to have_content(shelter_2.name)
-    # expect(page).to have_content("Name: #{shelter_2.name}")
+    visit "/shelters/#{shelter_2.id}"
+
+    expect(page).to have_content(shelter_2.name)
+    expect(page).to have_content("Address: #{shelter_2.address}")
+    expect(page).to have_content("City: #{shelter_2.city}")
+    expect(page).to have_content("State: #{shelter_2.state}")
+    expect(page).to have_content("Zip: #{shelter_2.zip}")
   end
 end
