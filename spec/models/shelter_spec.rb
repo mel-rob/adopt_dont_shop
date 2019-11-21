77e665dc61f85d836ac610291b919cf7d57a26dd
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-21 14:15:24 -0700

    Add test for adoptable_pets method in Shelter model

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -12,4 +12,42 @@ RSpec.describe Shelter, type: :model do
   describe "relationships" do
     it { should have_many :pets }
   end
+
+  describe 'methods' do
+   it "a user can see shelter's adoptable pets " do
+     shelter_1 = Shelter.create(name:     "Reptile Room",
+                                address:  "2364 Desert Lane",
+                                city:     "Denver",
+                                state:    "CO",
+                                zip:      "80211")
+
+
+     pet_1 = Pet.create(image: "https://cdn.shopify.com/s/files/1/0341/4893/products/joorvl5fxa7oxccvhj72.jpg?v=1553159130",
+                        name:  "Alfredo",
+                        desc:  "I'm a white ball python named Alfredo!",
+                        age:   "4",
+                        sex:   "female",
+                        status:"adoptable",
+                        shelter_id: shelter_1.id)
+
+     pet_2 = Pet.create(image: "https://www.geek.com/wp-content/uploads/2019/04/pantherchameleon1-625x352.jpg",
+                        name:  "Poppy",
+                        desc:  "I'm a panther chameleon! I am not very social but am fun to look at.",
+                        age:   "2",
+                        sex:   "male",
+                        status:"adoptable",
+                        shelter_id: shelter_1.id)
+
+    pet_3 = Pet.create(image:      "http://reptile.guide/wp-content/uploads/2019/02/Bearded-dragon-poop-.jpg",
+                        name:       "Chive",
+                        desc:       "I'm a bearded dragon. I like to hunt and be active during the daytime.",
+                        age:        "5",
+                        sex:        "male",
+                        status:     "pending adoption",
+                        shelter_id: shelter_1.id)
+
+    expect(shelter_1.adoptable_pets).to include(pet_1, pet_2)
+    expect(shelter_1.adoptable_pets).not_to include(pet_3)
+    end
+  end
 end
