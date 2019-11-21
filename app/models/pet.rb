8978ccdc898f520b037b3ea730be6879cf91ac32
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-20 20:13:45 -0700

    Add Pet model

---
+++ app/models/pet.rb
@@ -0,0 +1,11 @@
+class Pet < ApplicationRecord
+  belongs_to :shelter
+
+  validates_presence_of :image,
+                        :name,
+                        :desc,
+                        :age,
+                        :sex,
+                        :status
+
+end
