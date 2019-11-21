b18bcb3502829a27d5e3e525ee68e143e8e7940d
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-21 14:11:29 -0700

    Add route for get '/shelters/:id/pets/new', to 'pets#new'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#index'
   get '/shelters', to: 'shelters#index'
@@ -7,8 +6,13 @@ Rails.application.routes.draw do
   post '/shelters', to: 'shelters#create'
   get '/shelters/:id', to: 'shelters#show'
   get '/shelters/:id/edit', to: 'shelters#edit'
+  get '/shelters/:id/pets', to: 'pets#index'
+  post '/shelters/:id/pets', to: 'pets#create'
+  get '/shelters/:id/pets/new', to: 'pets#new'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
 
   get '/pets', to: 'pets#index'
+  get '/pets/:id', to: 'pets#show'
+  delete '/pets/:id', to: 'pets#destroy'
 end
