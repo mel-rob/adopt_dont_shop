f2ffdad594e62c3da534979d3102464b132b5cef
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-22 10:29:12 -0700

    Add test for User Story 14, user_can_update_pet_from_pet_index

---
+++ spec/features/usability/user_can_update_pet_from_pet_index_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "pet index page", type: :feature do
+  it "can update pet from pet index page" do
+
+  shelter_1 = Shelter.create(name:     "Reptile Room",
+                             address:  "2364 Desert Lane",
+                             city:     "Denver",
+                             state:    "CO",
+                             zip:      "80211")
+
+
+  pet_1 = Pet.create(image: "https://cdn.shopify.com/s/files/1/0341/4893/products/joorvl5fxa7oxccvhj72.jpg?v=1553159130",
+                    name:  "Alfredo",
+                    desc:  "I'm a white ball python named Alfredo!",
+                    age:   "4",
+                    sex:   "female",
+                    status:"adoptable",
+                    shelter_id: shelter_1.id)
+
+  visit '/pets'
+
+  expect(page).to have_link('Edit')
+
+  click_link 'Edit'
+
+  expect(current_path).to eq("/pets/#{pet_1.id}/edit")
+  
+  expect(page).to have_button('Submit')
+
+  fill_in 'name', with: "Spaghetti"
+
+  click_button 'Submit'
+  expect(current_path).to eq("/pets/#{pet_1.id}")
+
+  expect(page).to have_content("Spaghetti")
+  expect(page).not_to have_content("Name: Alfredo")
+  end
+end
