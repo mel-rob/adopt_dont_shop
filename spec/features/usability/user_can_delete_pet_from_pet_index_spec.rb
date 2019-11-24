d9241a077cb09a06e328298eb89b4a63112b7840
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-23 18:24:49 -0700

    Refactor test for user_can_delete_pet_from_pet_index

--- spec/features/usability/user_can_delete_pet_from_pet_index_spec.rb
+++ spec/features/usability/user_can_delete_pet_from_pet_index_spec.rb
@@ -1,32 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe "pet index page", type: :feature do
-  it "can delete pet from pet index" do
+  before(:each) do
 
-    shelter_1 = Shelter.create(name:     "Reptile Room",
+    @shelter_1 = Shelter.create(name:     "Reptile Room",
                                address:  "2364 Desert Lane",
                                city:     "Denver",
                                state:    "CO",
                                zip:      "80211")
 
-    pet_1 = Pet.create(image: "https://cdn.shopify.com/s/files/1/0341/4893/products/joorvl5fxa7oxccvhj72.jpg?v=1553159130",
+    @pet_1 = Pet.create(image: "https://cdn.shopify.com/s/files/1/0341/4893/products/joorvl5fxa7oxccvhj72.jpg?v=1553159130",
                        name:  "Alfredo",
                        desc:  "I'm a white ball python named Alfredo!",
                        age:   "4",
                        sex:   "female",
                        status:"adoptable",
-                       shelter_id: shelter_1.id)
+                       shelter_id: @shelter_1.id)
+
+   @pet_2 = Pet.create(image: "https://www.geek.com/wp-content/uploads/2019/04/pantherchameleon1-625x352.jpg",
+                       name:  "Poppy",
+                       desc:  "I'm a panther chameleon! I am not very social but am fun to look at.",
+                       age:   "2",
+                       sex:   "male",
+                       status:"adoptable",
+                       shelter_id: @shelter_1.id)
+  end
+
+  it "can delete pet from pet index" do
 
     visit "/pets"
 
-    expect(page).to have_button('Delete')
+    expect(page).to have_link('Delete', count: 2)
+  end
 
-    click_button 'Delete'
+  it "has link that when clicked deletes shelter" do
+
+    visit '/pets'
+
+    within "##{@pet_1.id}-section" do
+      click_link 'Delete'
+    end
 
     expect(current_path).to eq('/pets')
-    # expect(page.not_to find("#img-#{pet_1.id}")['src']).to have_content "#{pet_1.image}"
-    expect(page).not_to have_content(pet_1.name)
-    expect(page).not_to have_content(pet_1.age)
-    expect(page).not_to have_content(pet_1.sex)
+    expect(page).to_not have_content(@pet_1.name)
+    expect(page).to have_content(@pet_2.name)
     end
-end
+  end
