3426837f31357fb3b82cfaaa763c406915d2f71e
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-23 17:37:13 -0700

    Refactor test for user_can_delete_shelter_from_index

--- spec/features/usability/user_can_delete_shelter_from_index_spec.rb
+++ spec/features/usability/user_can_delete_shelter_from_index_spec.rb
@@ -1,14 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe "shelter index page", type: :feature do
-  it "can update shelter from shelter index page" do
-    shelter_1 = Shelter.create(name:     "Reptile Room",
+RSpec.describe "shelters index page", type: :feature do
+  before(:each) do
+    @shelter_1 = Shelter.create(name:     "Reptile Room",
                                address:  "2364 Desert Lane",
                                city:     "Denver",
                                state:    "CO",
                                zip:      "80211")
 
+   @shelter_2 = Shelter.create(name:    "Fish Farm",
+                              address: "8473 Ocean Drive",
+                              city:    "Denver",
+                              state:   "CO",
+                              zip:     "80232")
+  end
+
+  it "has link to delete each individual shelter" do
+
     visit '/shelters'
+
+    expect(page).to have_link('Delete', count: 2)
+  end
+
+  it "has link that when clicked deletes shelter" do
+
+    visit '/shelters'
+
+    within "##{shelter_1.id}-section" do
+      click_link 'Delete'
+    end
+
+    expect(current_path).to eq('/shelters')
+    expect(page).to_not have_content(@shelter_1.name)
+    expect(page).to have_content(@shelter_2.name)
 
     expect(page).to have_content("Reptile Room")
     expect(page).to have_button('Delete')
