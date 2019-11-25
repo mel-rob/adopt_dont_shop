5489bbf99bb79716901daa5bbfc274760d53b445
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-25 09:43:18 -0700

    Add test for User Story 21, Shelter Pet Index Link

---
+++ spec/features/usability/user_can_access_shelter_pets_index_from_shelter_show_page_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "shelters show page" do
+  before(:each) do
+
+  @shelter_1 = Shelter.create(name:     "Reptile Room",
+                             address:  "2364 Desert Lane",
+                             city:     "Denver",
+                             state:    "CO",
+                             zip:      "80211")
+  end
+
+  it "has a link to access shelter pets index" do
+
+    visit "/shelters/#{@shelter_1.id}"
+
+    expect(page).to have_link("All Pets at #{@shelter_1.name}")
+
+    click_link "All Pets at #{@shelter_1.name}"
+
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}/pets")
+  end
+end
