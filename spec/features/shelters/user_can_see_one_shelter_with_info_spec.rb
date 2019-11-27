a383bc087fb3d2dd5a52a8196ddc1a77df1e34ec
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-26 20:44:02 -0700

    Refactor test user_can_see_one_shelter_with_info

--- spec/features/shelters/user_can_see_one_shelter_with_info_spec.rb
+++ spec/features/shelters/user_can_see_one_shelter_with_info_spec.rb
@@ -1,33 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "shelter show page", type: :feature do
-  it "can see shelter with name address city state zip" do
-    shelter_1 = Shelter.create(name:     "Reptile Room",
-                               address:  "2364 Desert Lane",
+  before(:each) do
+    @shelter_1 = Shelter.create(name:     "Reptile Room",
+                               address:   "2364 Desert Lane",
+                               city:      "Denver",
+                               state:     "CO",
+                               zip:       "80211")
+
+    @shelter_2 = Shelter.create(name:    "Fish Farm",
+                               address:  "8473 Ocean Drive",
                                city:     "Denver",
                                state:    "CO",
-                               zip:      "80211")
+                               zip:      "80232")
+  end
 
-    shelter_2 = Shelter.create(name:    "Fish Farm",
-                               address: "8473 Ocean Drive",
-                               city:    "Denver",
-                               state:   "CO",
-                               zip:     "80232")
+  it "can see shelter with name address city state zip" do
 
-    visit "/shelters/#{shelter_1.id}"
+    visit "/shelters/#{@shelter_1.id}"
 
-    expect(page).to have_content(shelter_1.name)
-    expect(page).to have_content("Address: #{shelter_1.address}")
-    expect(page).to have_content("City: #{shelter_1.city}")
-    expect(page).to have_content("State: #{shelter_1.state}")
-    expect(page).to have_content("Zip: #{shelter_1.zip}")
+    expect(page).to have_content(@shelter_1.name)
+    expect(page).to have_content("Address: #{@shelter_1.address}")
+    expect(page).to have_content("City: #{@shelter_1.city}")
+    expect(page).to have_content("State: #{@shelter_1.state}")
+    expect(page).to have_content("Zip: #{@shelter_1.zip}")
+  end
 
-    visit "/shelters/#{shelter_2.id}"
+  it "can see another shelter with name address city state zip" do
 
-    expect(page).to have_content(shelter_2.name)
-    expect(page).to have_content("Address: #{shelter_2.address}")
-    expect(page).to have_content("City: #{shelter_2.city}")
-    expect(page).to have_content("State: #{shelter_2.state}")
-    expect(page).to have_content("Zip: #{shelter_2.zip}")
+    visit "/shelters/#{@shelter_2.id}"
+
+    expect(page).to have_content(@shelter_2.name)
+    expect(page).to have_content("Address: #{@shelter_2.address}")
+    expect(page).to have_content("City: #{@shelter_2.city}")
+    expect(page).to have_content("State: #{@shelter_2.state}")
+    expect(page).to have_content("Zip: #{@shelter_2.zip}")
   end
 end
