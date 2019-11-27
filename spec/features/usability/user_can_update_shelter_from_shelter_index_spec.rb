4c3538395f703186f91de3db689d086684265ec1
Melissa Robbins <melissamrobbins@gmail.com>
2019-11-26 20:47:02 -0700

    Refactor test user_can_update_shelter_from_shelter_index

--- spec/features/usability/user_can_update_shelter_from_shelter_index_spec.rb
+++ spec/features/usability/user_can_update_shelter_from_shelter_index_spec.rb
@@ -1,22 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe "shelter index page", type: :feature do
-  it "can update shelter from shelter index page" do
-    shelter_1 = Shelter.create(name:     "Reptile Room",
+RSpec.describe "shelters index page", type: :feature do
+  before(:each) do
+    @shelter_1 = Shelter.create(name:     "Reptile Room",
                                address:  "2364 Desert Lane",
                                city:     "Denver",
                                state:    "CO",
                                zip:      "80211")
 
+
+    @shelter_2 = Shelter.create(name:    "Fish Farm",
+                               address: "8473 Ocean Drive",
+                               city:    "Denver",
+                               state:   "CO",
+                               zip:     "80232")
+  end
+
+  it "has link to edit each shelter from shelters index page" do
+
     visit '/shelters'
 
-    expect(page).to have_content("Reptile Room")
-    expect(page).to have_link('Create New')
-    expect(page).to have_link('Edit')
+    expect(page).to have_content(@shelter_1.name)
+    expect(page).to have_content(@shelter_2.name)
 
-    # click_link 'Edit'
+    within "##{@shelter_1.id}-section" do
+      expect(page).to have_link('Edit')
+    end
 
-    # expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+    within "##{@shelter_2.id}-section" do
+      expect(page).to have_link('Edit')
+    end
+  end
 
+  it "has link to edit each shelter which when clicked goes to shelter edit form" do
+
+    visit '/shelters'
+
+    within "##{@shelter_1.id}-section" do
+      click_link 'Edit'
+    end
+
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}/edit")
   end
 end
